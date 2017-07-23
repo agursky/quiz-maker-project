64f6031a23faaa61b48c462b35d856686faa0a10
Andrew Gursky <agursky.js@gmail.com>
2017-07-23 15:16:33 -0400

    Adding router file and router nav.

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,4 @@
 var app = 
-    angular.module('quizzer', []);
+    angular.module('quizzer', ["ngRoute"]);
 
 
---
+++ js/router.js
@@ -0,0 +1,6 @@
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/take", {
+        templateUrl: "pages/take-quiz.html"
+    })
+})
