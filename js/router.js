baa7a563db87dca2d22b3d055a32bb7990111762
Andrew Gursky <agursky.js@gmail.com>
2017-07-23 15:31:44 -0400

    Added routes for each page. Added routes to main nav.

--- js/router.js
+++ js/router.js
@@ -1,6 +1,22 @@
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/take", {
+    .when("/home", {
+        templateUrl: "pages/teacher-dash.html"
+    })
+    .when("/take-quiz", {
         templateUrl: "pages/take-quiz.html"
     })
+    .when("/make-quiz", {
+        templateUrl: "pages/make-quiz.html"
+    })
+    .when("/view-scores", {
+        templateUrl: "pages/teacher-dash.html"
+    })
+    .when("/teacher-dash", {
+        templateUrl: "pages/teacher-dash.html"
+    })
+    .otherwise({
+        templateUrl: "pages/teacher-dash.html"
+    })
+    
 })
